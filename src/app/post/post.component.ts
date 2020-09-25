aafd8088598aff09791aa06497fc7b28529c9b0f
Berk Soroğlu <berksoroglu@gmail.com>
2020-09-25 13:02:09 +0300

    Update post.component.ts
    ActivatedRoute settings

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Post } from './post';
 import { user } from './users';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-post',
@@ -11,23 +12,34 @@ import { HttpClient } from '@angular/common/http';
 
 export class PostComponent implements OnInit {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient, private activatedRoute:ActivatedRoute) { }
 
   path:string="https://jsonplaceholder.typicode.com/";
   post:Post[];
   users:user[];
 
   ngOnInit(): void {
-    this.get_posts();
+    this.activatedRoute.params.subscribe( params => {
+      this.get_posts(params["userid"])
+    } );
     this.get_users();
 
   }
 
-  get_posts() {
+  get_posts(userid) {
+
+    if(userid) {
+
+      this.http.get<Post[]>(this.path+"posts?userId="+userid).subscribe(Response=>{
+        this.post = Response;
+      }) 
+
+    } else {
     this.http.get<Post[]>(this.path+"posts").subscribe(Response=>{
       this.post = Response;
     }) 
   }
+}
 
   get_users() {
     this.http.get<user[]>(this.path+"users").subscribe(Response=>{
